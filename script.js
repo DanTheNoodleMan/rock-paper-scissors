b6d8f9c2c571a0505374c3c0f59b491f77724baa
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-10-29 23:37:11 +0200

    Made basic rock paper scissors game playable in console asking for user prompt that plays 5 times

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let possible = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice() {
+    let choice = possible[Math.floor(Math.random() * possible.length)];
+    return choice;
+}
+
+
+
+function playRound(playerSel, computerSel) {
+    if(playerSel == computerSel){
+        return "It's a tie, you're mediocre"
+
+    } else if (playerSel == 'rock'  &&	computerSel == 'paper'){
+        return "LOSER: Your rock got folded by paper, ironic huh?"
+    } else if (playerSel == 'rock'  &&	computerSel == 'scissors'){
+        return "WINNER: Your rock crushed sharp scissors"
+
+    } else if (playerSel == 'paper'  &&	computerSel == 'rock'){
+        return "WINNER: You folded a rock with paper, irony is a funny thing"
+    } else if (playerSel == 'paper'  &&	computerSel == 'scissors'){
+        return "LOSER: Chop"
+
+    } else if (playerSel == 'scissors'  &&	computerSel == 'rock'){
+        return "LOSER: Don't bring scissors to a rock fight"
+    } else if (playerSel == 'scissors'  &&	computerSel == 'paper'){
+        return "Winner: Chop (but good)"
+    } 
+}
+
+
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        let playerSel = prompt("Make your move!").toLowerCase();
+        let computerSel = getComputerChoice().toLowerCase();
+        console.log(playRound(playerSel, computerSel));
+    }
+}
